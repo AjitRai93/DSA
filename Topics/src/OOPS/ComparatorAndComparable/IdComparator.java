f97dc8d6ee3bc09276926c5afc55dbcb5e52477a
Ajit <ajit15.rai@gmail.com>
2024-01-19 22:37:08 +0530

    Modified Comparator logic

--- Topics/src/OOPS/ComparatorAndComparable/IdComparator.java
+++ Topics/src/OOPS/ComparatorAndComparable/IdComparator.java
@@ -7,7 +7,8 @@ public class IdComparator implements Comparator<Student> {
     @Override
     public int compare(Student s1, Student s2) {
         if(s1.getId() == s2.getId()){
-            return 0;
+            //Suppose if we have two ids that matches then we can sort those records with name else return 0;
+            return s1.getName().compareTo(s2.getName());
         } else if (s1.getId() > s2.getId()) {
             return 1;
         }else {
